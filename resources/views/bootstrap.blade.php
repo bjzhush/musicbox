8e7c98687c3f4721de33fa661c6be69bad5a531c
bjzhush <bjzhush@gmail.com>
2016-10-11 01:29:17 +0800

    add nav

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -1,1 +1,30 @@
 <link href="css/bootstrap.min.css" rel="stylesheet">
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">我的音乐盒</a>
+        </div>
+        <div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">上传</a></li>
+                <li><a href="#">标记</a></li>
+                <li><a href="#">播放</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        其它
+                        <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/test">jmeter</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">分离的链接</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
