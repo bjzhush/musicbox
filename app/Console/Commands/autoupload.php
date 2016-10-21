bdc0afa7f24e1fc0f261a73d1e549dc2e16a17ec
zhushuai <zhushuai@allydata.cn>
2016-10-21 18:11:46 +0800

    增加错误提示等

--- app/Console/Commands/autoupload.php
+++ app/Console/Commands/autoupload.php
@@ -28,12 +28,15 @@ class autoupload extends Command
      */
     public function handle()
     {
+        $startDay = date('Y-m-d');
+        $this->info('start running '.date('Y-m-d H:i:s'));
         $config = [
             'userId' => env('AUTO_UPLOAD_USER_ID'),
             'uploadUrl' => env('AUTO_UPLOAD_URL'),
             'uploadAuthKey' => env('AUTO_UPLOAD_AUTH_KEY'),
             'localDir' => env('UPLOAD_LOCAL_DIR'),
             'uploadedDir' => env('UPLOADED_DIR'),
+            'failedDir' => env('FAILED_DIR'),
         ];
         
         foreach ($config as $k =>  $item) {
@@ -44,31 +47,78 @@ class autoupload extends Command
         }
         
         try {
-            $musicFiles = scandir($config['localDir']);
-            if (count($musicFiles)) {
-                foreach($musicFiles as $file) {
-                    $uploadResponse = $this->uploadMusic($file, $config);
-                    $this->info($uploadResponse);
+            while(date('Y-m-d') == $startDay) {
+                $musicFiles = scandir($config['localDir']);
+                foreach ($musicFiles as $k => $musicFile) {
+                   if (in_array($musicFile, ['.','..']))  {
+                      unset($musicFiles[$k]);
+                   }
                 }
                 
-            } else {
-               sleep(200);
-               $this->info('no task,sleeping');
+                if (count($musicFiles)) {
+                    foreach($musicFiles as $file) {
+                        $resJson = $this->uploadMusic($file, $config);
+                        $resArr = json_decode($resJson, TRUE);
+                        if (!is_null($resArr) && isset($resArr['code']) && $resArr['code'] == 200) {
+                            $mv = rename($config['localDir'].'/'.$file, $config['uploadedDir'].'/'.$file);
+                            if ($mv) {
+                                $this->info('succeed upload '.$file);
+                            } else {
+                                $errorInfo = 'error moving file';
+                                $this->error($errorInfo.$file);
+                                $this->sendDesktopNotify($errorInfo);
+                                exit;
+                            }
+                        } else {
+                            $this->error('response: '.$resJson);
+                            $this->sendDesktopNotify($resJson);
+                            exit;
+                        }
+                    }
+
+                } else {
+                    $this->info('no task,sleeping 600');
+                    sleep(600);
+                }
             }
             
         } catch (\Exception $e) {
+            echo $e->getMessage();
+            $this->sendDesktopNotify($e->getMessage());
             \App\Libs\LogService::logWrite('exceptionlog', $e->getMessage());
         }
         
-        $this->info('running');
+        $this->info('finish running '.date('Y-m-d H:i:s'));
     }
     
     public function uploadMusic($file, $config)
     {
         if (!in_array($file, ['.','..'])) {
-            echo "<pre>";
-            var_dump($file);
+            $file = $config['localDir'].'/'.$file;
+
+            if (function_exists('curl_file_create')) { // php 5.6+
+                $cFile = curl_file_create($file);
+            } else { //
+                $cFile = realpath($file);
+            }
+            $post = array(
+                'userid' => $config['userId'],
+                'authkey' => $config['uploadAuthKey'],
+                'mfile'=> $cFile
+            );
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$config['uploadUrl']);
+            curl_setopt($ch, CURLOPT_POST,1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            $result=curl_exec ($ch);
+            curl_close ($ch);
+            return $result;
         }
-        
+
+    }
+    
+    public function sendDesktopNotify($content) {
+        file_put_contents('/tmp/notify_box.sh',"DISPLAY=:0.0 notify-send 'Allydata Exception' '".$content." ".date('Y-m-d H:i:s')."'".PHP_EOL, FILE_APPEND);
     }
 }
